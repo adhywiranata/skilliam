78976b179fffedda94bf5a99ea8b0b800fd4215a
Adhy <adhywiranatap@gmail.com>
2017-12-28 22:24:31 +0700

    implement card to homepage

--- src/Views/Pages/Home.elm
+++ src/Views/Pages/Home.elm
@@ -5,9 +5,17 @@ import Models exposing (Model)
 
 import Msgs exposing (Msg)
 
+import Molecules.Card as Card
+import Atoms.H1 as H1
+import Atoms.H2 as H2
+import Atoms.Text as Text
+
 view : Model -> Html Msg
 view model =
     div
         []
-        [ h1 [] [ text "Home Page" ]
+        [ H1.view "Home Page"
+        , Card.view (div [][ H2.view "hello", Text.view "lorem ipsum"])
+        , Card.view (div [][ H2.view "hola", Text.view "hoi"])
+        , Card.view (div [][ H2.view "yeah", Text.view "awesome"])
         ]
